0cecadb40e93065a381ccd217834e73bbf37a901
Karan <karanchandravanshi217@gmail.com>
2023-09-15 23:30:56 +0530

    Updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import '../styles/globals.css'
+import { Analytics } from '@vercel/analytics/react';
+import { H } from 'highlight.run';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  H.init('jdk7xvg5', {
+    environment: 'production',
+    enableStrictPrivacy: false,
+    networkRecording: {
+      enabled: true,
+      recordHeadersAndBody: true,
+    },
+    tracingOrigins: true
+  });
+  return <>
+      <Component {...pageProps} />
+      <Analytics />
+  </>
 }
