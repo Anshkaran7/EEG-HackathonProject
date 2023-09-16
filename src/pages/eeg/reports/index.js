4959bc2ed6e5202d722a6ed56f8841a0926f4944
Karan <karanchandravanshi217@gmail.com>
2023-09-16 10:59:59 +0530

    update

--- src/pages/eeg/reports/index.js
+++ src/pages/eeg/reports/index.js
@@ -1,5 +1,6 @@
 import { IconMessageCircle } from "@tabler/icons-react";
 import Navigation from "../../../Components/Navigation";
+import EEGGraph from "../../../Components/Chart";
 
 const Reports = () => {
     return (
@@ -8,14 +9,17 @@ const Reports = () => {
                 <Navigation type="doctor" activeTab={"Reports"} />
             </div>
 
-            <div className=" bg-white w-full rounded-lg shadow-md m-6  flex">
+            <div className=" flex-col bg-white w-full rounded-lg shadow-md m-6  flex">
                 <p className="text-base m-6  text-theme-gray-dark font-medium">Reports</p>
 
+                <div className="flex justify-center items-center h-screen">
+                    <EEGGraph />
+                </div>
             </div>
 
             <button className="fixed flex bottom-8 right-8 p-2 border-2 border-red-400 rounded-full shadow-md">
-        <IconMessageCircle className="text-red-400" />
-      </button>
+                <IconMessageCircle className="text-red-400" />
+            </button>
         </div>
     );
 }
