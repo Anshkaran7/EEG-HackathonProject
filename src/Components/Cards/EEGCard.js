4268e3cbbcaa8346014b9076c07a20dba6c09bf8
Karan <karanchandravanshi217@gmail.com>
2023-09-16 10:47:52 +0530

    pusshhhhh

--- src/Components/Cards/EEGCard.js
+++ src/Components/Cards/EEGCard.js
@@ -4,15 +4,11 @@ import React from 'react';
 
 const EEGCard = () => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg mx-auto mt-10">
+    <div className="max-w-sm rounded overflow-hidden shadow-theme-red shadow-md bg-[#FFB1C1] mx-auto mt-10">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2 text-theme-gray-dark">Please Connect to EEG</div>
       </div>
-      <div className="px-6 py-4">
-        <span className="bg-blue-500 text-white py-1 px-2 rounded-full text-xs">
-          Blinking
-        </span>
-      </div>
+      
     </div>
   );
 };
--- src/Components/MemoryGame.js
+++ src/Components/MemoryGame.js
@@ -1,20 +1,15 @@
 import React, { useState, useEffect } from 'react';
-// import elephantImage from './images/elephant.png';
-// import lionImage from './images/lion.png';
-// import monkeyImage from './images/monkey.png';
-// import giraffeImage from './images/girafe.png';
-// import zebraImage from './images/zebra.png';
-// import tigerImage from './images/tiger.png';
+
 
 function MemoryGame() {
-    const animals = [
-        { id: 1, name: 'Elephant', image: "/images/elephant.png" },
-        { id: 2, name: 'Lion', image: "/images/lion.png" },
-        { id: 3, name: 'Monkey', image: "/images/monkey.png" },
-        { id: 4, name: 'Giraffe', image: "/images/girafe.png" },
-        { id: 5, name: 'Zebra', image: "/images/zebra.png" },
-        { id: 6, name: 'Tiger', image: "/images/tiger.png" },
-      ];
+  const animals = [
+    { id: 1, name: 'Elephant', image: "/images/elephant.png" },
+    { id: 2, name: 'Lion', image: "/images/lion.png" },
+    { id: 3, name: 'Monkey', image: "/images/monkey.png" },
+    { id: 4, name: 'Giraffe', image: "/images/girafe.png" },
+    { id: 5, name: 'Zebra', image: "/images/zebra.png" },
+    { id: 6, name: 'Tiger', image: "/images/tiger.png" },
+  ];
 
   const [cards, setCards] = useState([]);
   const [flippedIndexes, setFlippedIndexes] = useState([]);
@@ -83,25 +78,24 @@ function MemoryGame() {
 
   return (
     <div className="container mx-auto max-w-xl mt-10">
-      <h1 className="text-3xl font-bold mb-4">Memory Game</h1>
-      <div className="grid grid-cols-3 gap-4">
-      {cards.map((animal, index) => (
-  <div
-    key={index}
-    className={`bg-blue-200 rounded-md h-32 flex justify-center items-center cursor-pointer transition-transform transform hover:scale-105 ${
-      flippedIndexes.includes(index) || matchedPairs.includes(animal.name)
-        ? 'bg-white'
-        : ''
-    }`}
-    onClick={() => handleCardClick(index)}
-  >
-    {flippedIndexes.includes(index) || matchedPairs.includes(animal.name) ? (
-      <img src={animal.image} alt={animal.name} className="w-32 h-32 object-cover" />
-    ) : (
-      <span>?</span>
-    )}
-  </div>
-))}
+      <h1 className="text-3xl font-bold mb-4 text-theme-gray-dark">Memory Game</h1>
+      <div className="grid grid-cols-3 gap-4" style={{ maxHeight: '400px', overflowY: 'scroll' }}>
+        {cards.map((animal, index) => (
+          <div
+            key={index}
+            className={`bg-blue-200 rounded-md h-20 flex justify-center items-center cursor-pointer transition-transform transform hover:scale-105 ${flippedIndexes.includes(index) || matchedPairs.includes(animal.name)
+                ? 'bg-white'
+                : ''
+              }`}
+            onClick={() => handleCardClick(index)}
+          >
+            {flippedIndexes.includes(index) || matchedPairs.includes(animal.name) ? (
+              <img src={animal.image} alt={animal.name} className="w-20 h-20 object-cover" />
+            ) : (
+              <span>?</span>
+            )}
+          </div>
+        ))}
 
       </div>
       <button
